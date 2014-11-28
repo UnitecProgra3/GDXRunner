3895aa0d404621102026d8f2ca221282ba08f6bc
Turupawn <ahmed.hn.43@gmail.com>
2014-11-28 11:14:11 -0600

    primer commit

---
+++ core/src/lab/progra2/prueba/ClasePrincipal.java
@@ -0,0 +1,94 @@
+package lab.progra2.prueba;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+public class ClasePrincipal extends ApplicationAdapter implements InputProcessor{
+	Stage stage;
+	int frame = 0;
+	Jugador jugador;
+	
+	@Override
+	public void create () {		
+		stage=new Stage();
+		Gdx.input.setInputProcessor(this);
+		
+		jugador = new Jugador();
+		stage.addActor(jugador);
+		stage.addActor(new Enemigo(jugador));
+		stage.addActor(new Plataforma());
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(0f, 1, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		stage.draw();
+		stage.act();
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		jugador.saltar();
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
+
+
+
+
+
+
+
+
